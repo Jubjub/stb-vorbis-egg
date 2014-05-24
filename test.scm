acd89f3b7936ddac463cee5f01cf610b773c858c
Jubjub <ethedona@gmail.com>
2014-05-24 14:47:15 +0200

    fix example

--- test.scm
+++ test.scm
@@ -4,9 +4,10 @@
 (use srfi-4)
 (use stb-vorbis)
 
+(use lolevel)
 (define device (alc:OpenDevice #f))
 (define context (alc:CreateContext device #f))
-(define data (stbv:decode-file "test.ogg"))
+(define data (car (stbv:decode-file "test.ogg")))
 
 (define (sound i)
   (/ (modulo i 60) 60))
@@ -17,7 +18,7 @@
   (loop (+ 1 i))))
 
 (alc:MakeContextCurrent context)
-(define buffer (openal:make-buffer data #f 44100))
+(define buffer (openal:make-buffer data #t 44100))
 (define source (openal:make-source buffer))
 
 (al:Sourcei source al:LOOPING 1)
