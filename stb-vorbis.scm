8d34925f369658b4ab00983d701ca7cc38c80342
Jubjub <ethedona@gmail.com>
2014-05-24 15:30:43 +0200

    basic pulldata api

--- stb-vorbis.scm
+++ stb-vorbis.scm
@@ -6,11 +6,50 @@
 (module stb-vorbis *
         (import chicken scheme foreign lolevel)
         (use srfi-4)
+
+        (define stb:vorbis-open-filename
+          (foreign-lambda* c-pointer ((c-string path))
+            "C_return(stb_vorbis_open_filename(path, 0, 0));"))
+
+        #;(define stb:vorbis-get-info
+          (foreign-lambda* c-pointer ((c-pointer object))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_get_info(stb_vorbis_object));"))
+
+        (define stb:vorbis-sample-rate
+          (foreign-lambda* integer ((c-pointer object))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_object->sample_rate);"))
+
+        (define stb:vorbis-channels
+          (foreign-lambda* integer ((c-pointer object))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_object->channels);"))
+
+        (define stb:vorbis-stream-length-in-samples
+          (foreign-lambda* integer ((c-pointer object))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_stream_length_in_samples(object));"))
+
+        (define stb:vorbis-stream-length-in-seconds
+          (foreign-lambda* float ((c-pointer object))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_stream_length_in_seconds(object));"))
+
+        (define stb:vorbis-get-samples-short-interleaved
+          (foreign-lambda* integer ((c-pointer object)
+                                    (integer channels)
+                                    (c-pointer buffer)
+                                    (integer num_shorts))
+            "stb_vorbis *stb_vorbis_object = object;
+            C_return(stb_vorbis_get_samples_short_interleaved(stb_vorbis_object, channels,
+                                                              buffer, num_shorts));"))
+
         (define stb:vorbis-decode-filename
           (foreign-lambda* c-pointer ((c-string path)
                                       ((c-pointer integer) channels)
                                       ((c-pointer integer) size))
-          "short *buffer;
+            "short *buffer;
           int read = stb_vorbis_decode_filename(path, channels, &buffer);
           *size = read;
           C_return(buffer);"))
--- test.scm
+++ test.scm
@@ -4,21 +4,31 @@
 (use srfi-4)
 (use stb-vorbis)
 
-(use lolevel)
 (define device (alc:OpenDevice #f))
 (define context (alc:CreateContext device #f))
-(define data (car (stbv:decode-file "test.ogg")))
+(define ogg (stb:vorbis-open-filename "broken.ogg"))
+;(define data (car ogg))
+(define rate (stb:vorbis-sample-rate ogg))
+(define channels (stb:vorbis-channels ogg))
+(define samples (stb:vorbis-stream-length-in-samples ogg))
+(print "loaded ogg with " channels " channels, sample rate:" rate " "
+       samples " samples, " (stb:vorbis-stream-length-in-seconds ogg) " seconds")
+
+(define data (make-s16vector (* channels (stb:vorbis-stream-length-in-samples ogg))))
+(define samples-read
+  (stb:vorbis-get-samples-short-interleaved ogg channels (make-locative data) (* samples channels)))
+(print "read " samples-read " samples")
 
 (define (sound i)
   (/ (modulo i 60) 60))
 
 #;(let loop ((i 0))
-  (unless (>= i 600000)
+(unless (>= i 600000)
   (s16vector-set! data i (inexact->exact (floor (max 0 (min 256 (* 256 (sound i)))))))
   (loop (+ 1 i))))
 
 (alc:MakeContextCurrent context)
-(define buffer (openal:make-buffer data #t 44100))
+(define buffer (openal:make-buffer data (= channels 2) rate))
 (define source (openal:make-source buffer))
 
 (al:Sourcei source al:LOOPING 1)
